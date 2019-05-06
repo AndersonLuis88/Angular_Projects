2ee053a322b261e0c910c11c8c1836a4ab760c51
Anderson <andersonluis1486@gmail.com>
2019-05-06 16:49:10 -0300

    Finish Banner

--- App_Instagram/app01/src/app/acesso/banner/banner.component.ts
+++ App_Instagram/app01/src/app/acesso/banner/banner.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, } from '@angular/core';
-import { style, trigger, state, } from '@angular/animations';
-
+import { style, trigger, state, transition, animate} from '@angular/animations';
+import {Imagem} from './image.model'
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
@@ -12,16 +12,45 @@ import { style, trigger, state, } from '@angular/animations';
       })),
       state('visivel', style({
         opacity: 1
-      }))
+      })),
+      transition('escondido <=> visivel', animate('1s ease-in')),
+      
     ])
   ]
 })
 export class BannerComponent implements OnInit {
 
-  public estado: string =  'escondido'
+
+  public estado: string =  'visivel'
+
+  public imagens : Imagem[] = [
+    {estado:'visivel', url:'/assets/banner-acesso/img_1.png'},
+    {estado:'escondido', url:'/assets/banner-acesso/img_2.png'},
+    {estado:'escondido', url:'/assets/banner-acesso/img_3.png'},
+    {estado:'escondido', url:'/assets/banner-acesso/img_4.png'},
+    {estado:'escondido', url:'/assets/banner-acesso/img_5.png'}
+  ]
+
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => this.logicaRotacao(),2000)
   }
+  public logicaRotacao(): void{
+    console.log(this.imagens)
+
+    let ix:number
+    for(let i:number = 0; i <=4;i++){
+      if(this.imagens[i].estado === 'visivel'){
+        this.imagens[i].estado = 'escondido'
+
+        ix = i === 4 ? 0 : i + 1
+        break
+      }
+    }
+    this.imagens[ix].estado = 'visivel'
+    setTimeout(() => this.logicaRotacao(),2000)
+  }
+  
 
 }
