303f0bd68887f2d5087eac4a8f334da7f3f2222b
Anderson <andersonluis1486@gmail.com>
2019-05-06 08:44:39 -0300

    Animations, Triggers, States and Styles

--- App_Instagram/app01/src/app/acesso/banner/banner.component.ts
+++ App_Instagram/app01/src/app/acesso/banner/banner.component.ts
@@ -1,9 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { style, trigger, state, } from '@angular/animations';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.styl']
+  styleUrls: ['./banner.component.styl'],
+  animations: [
+    trigger('banner',[
+      state('escondido',style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      }))
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
 
