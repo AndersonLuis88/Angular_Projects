7ba311d9f03db1fdf7440208210e050d35eee133
Anderson <andersonluis1486@gmail.com>
2019-05-07 05:39:17 -0300

    Create styles of transition

--- App_Instagram/app01/src/app/acesso/acesso.component.ts
+++ App_Instagram/app01/src/app/acesso/acesso.component.ts
@@ -1,12 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, state, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.styl']
+  styleUrls: ['./acesso.component.styl'],
+  animations: [
+    trigger('animacao-banner',[
+      state('criado',style({
+        opacity:1
+      })),
+      transition('void=> criado',[
+        style({opacity: 0, transform: 'translate(-800px,0)'}),
+        animate('500ms 1s ease-in-out')
+      ])
+    ]),//End Trigger Banner
+    trigger('animacao-cadastro',[
+      state('criado',style({
+        opacity:1
+      })),
+      transition('void=> criado',[
+        style({opacity: 0, transform: 'translate(800px,0)'}),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])//End Trigger Cadastro
+  ]
 })
 export class AcessoComponent implements OnInit {
 
+  public estadoBanner : string = 'criado'
   constructor() { }
 
   ngOnInit() {
